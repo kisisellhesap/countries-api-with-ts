81c24f1b5b00f547b19af16d021d716e4bf3d932
kisisellhesap <ahmetcannkaragoz@gmail.com>
2025-03-17 23:23:52 +0300

    update

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -4,6 +4,8 @@ import { useParams } from "react-router-dom";
 const Detail: FC = () => {
   const { name } = useParams();
 
+  console.log(name);
+
   return <div className="container">detail</div>;
 };
 
